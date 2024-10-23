95a33e967ad61c8c0863fda1e9d3e92b1f84e615
José Carlos Quintero Cedeño <jose.quinterocedeno@ucr.ac.cr>
2024-10-22 18:33:49 -0600

    fix: simplificar código de carga de la base de datos

--- config/carga_de_datos.R
+++ config/carga_de_datos.R
@@ -20,35 +20,20 @@ library(kableExtra)
 #Codigo para la lectura de la base de datos
 ENAHO <- read_sav("data/GPES-ELAB-GEBD-ENAHO-2023_BdBasePublica.sav")
 
-#Codigo para convertir las columnas categóricas a factor
-ENAHO$REGION <- as_factor(ENAHO$REGION)
-ENAHO$ZONA <- as_factor(ENAHO$ZONA)
-ENAHO$A3 <- as_factor(ENAHO$A3) #parentesco
-ENAHO$A4 <- as_factor(ENAHO$A4) #Sexo
-ENAHO$CondMig <- as_factor(ENAHO$CondMig) #condicion de migrante
-ENAHO$A15A <- as_factor(ENAHO$A15A) #tipo centro educativo
-ENAHO$A15B <- as_factor(ENAHO$A15B) #universidad publica
-ENAHO$NivInst <- as_factor(ENAHO$NivInst) #Nivel de instrucción
-ENAHO$Escolari <- as.integer(ENAHO$Escolari)
-ENAHO$Escolari[ENAHO$Escolari == 99] <- NA #años de escolaridad
-ENAHO$ForReg <- as_factor(ENAHO$ForReg) #formacion educativa formal
-ENAHO$A16B <- as_factor(ENAHO$A16B) #titulo
-ENAHO$ForNoReg <- as_factor(ENAHO$ForNoReg) #formacion educativa no formal
-ENAHO$A22A <- as_factor(ENAHO$A22A) #Dominio de segundo idioma
-ENAHO$Estabili <- as_factor(ENAHO$Estabili) #Estabilidad laboral
-ENAHO$InsLab <- as_factor(ENAHO$InsLab) #Satisfaccion laboral
-ENAHO$np <- as_factor(ENAHO$np) #Nivel de pobreza
-ENAHO$IPM_Pobreza <- as_factor(ENAHO$IPM_Pobreza) #nivel de pobreza multidimensional
-ENAHO$Q_IPCN <- as_factor(ENAHO$Q_IPCN) #Quintil de ingreso per capita neto
+# Convertir columnas categóricas a factores y ajustar años de escolaridad
+cols_to_factor <- c("ZONA", "A15A", "A15B", 
+                    "NivInst", "ForReg", "A16B", "A22A", 
+                    "Estabili", "InsLab", "np", "IPM_Pobreza", "Q_IPCN")
+ENAHO[cols_to_factor] <- lapply(ENAHO[cols_to_factor], as_factor)
+ENAHO$Escolari <- ifelse(ENAHO$Escolari == 99, NA, as.integer(ENAHO$Escolari))
 
 #filtrar variables de interés
-ENAHO <- ENAHO %>% select(REGION, ZONA,
-                          A3, A4, 
-                          A5, CondMig,
+ENAHO <- ENAHO %>% select(ZONA,
+                          A5,
                           A15A, A15B,
                           NivInst, Escolari,
                           ForReg, A16B,
-                          ForNoReg, A22A,
+                          A22A,
                           Estabili, InsLab,
                           itpn, lp, 
                           np, IPM_Pobreza,
